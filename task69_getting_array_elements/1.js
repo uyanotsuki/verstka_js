4e02349790bb4afc945c8a8fcc9b569720c485da
kageyoshi <diana.azamatovva@gmail.com>
2023-10-14 18:40:38 +0300

    Операции с элементами массива

---
+++ task69_getting_array_elements/1.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// 1 Создайте массив с элементами 1, 2, 3. Выведите на экран каждый из этих элементов
+
+let arr = [1, 2, 3];
+alert(arr[0]);
+alert(arr[1]);
+alert(arr[2]);
+
+
+// 2
+let arr2 = [1, 2, 3];
+alert(arr2[0]);
+alert(arr2[1]);
+alert(arr2[2]);
+
+// 3 сумма элементов
+let arr3 = [1, 2, 3];
+alert(Number(arr3[0])+Number(arr3[1])+Number(arr3[2]));
+
+// 4
+let arr4 = ['a', 'b', 'c', 'd'];
+alert(arr4[0] + "+" + arr4[1] + "+" + arr4[2] + "+" + arr4[3]);
