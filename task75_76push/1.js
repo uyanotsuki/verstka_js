24141305a2023c442d25c82985e1274d48193490
kageyoshi <diana.azamatovva@gmail.com>
2023-10-14 19:47:20 +0300

    Длина массива, добавление элементов в конец (push)

---
+++ task75_76push/1.js
@@ -0,0 +1,20 @@
+"use strict";
+// 1 (length = 9)
+let arr = [];
+arr[3] = 'a';
+arr[8] = 'b';
+alert(arr.length);
+
+// 1 (75)
+let none = [];
+none.push(1);
+none.push(2);
+none.push(3);
+alert(none);
+
+
+// 2
+let arr2 = [1, 2, 3];
+arr2.push(4);
+arr2.push(5);
+alert(arr2);
