584a1ddfa2fafabc5a0a741f48b403ede749fa11
kageyoshi <diana.azamatovva@gmail.com>
2023-10-15 01:06:33 +0300

    Object

---
+++ task80_obj/1.js
@@ -0,0 +1,4 @@
+"use strict";
+let obj = {1: 'monday', 2: 'tuesday', 3: 'wednesday', 4: 'thursday', 5: 'friday', 6: 'saturday', 7: 'sunday'};
+
+alert(obj[1] + ' ' + obj[2] + ' ' + obj[3] + ' ' + obj[4] + ' ' + obj[5] + ' ' + obj[6] + ' ' + obj[7]);
