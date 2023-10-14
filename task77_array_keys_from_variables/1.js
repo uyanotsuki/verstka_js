195a1a27221b90c371bf4eda6537294abddb8d4a
kageyoshi <diana.azamatovva@gmail.com>
2023-10-15 00:52:42 +0300

    Ключ массива как индекс, оператор Delete

---
+++ task77_array_keys_from_variables/1.js
@@ -0,0 +1,11 @@
+"use strict";
+// 1
+let arr = ['a', 'b', 'c'];
+let key = 2;
+alert(arr[key]);
+
+// 2 Hайдите сумму элементов, ключи которых хранятся в наших переменных
+let arr2 = [1, 2, 3, 4, 5];
+let key1 = 1;
+let key2 = 2;
+alert(Number(arr2[key1]) + Number(arr2[key2])); 
---
+++ task78_delete/1.js
@@ -0,0 +1,5 @@
+"use strict";
+let arr = ['a', 'b', 'c', 'd', 'e'];
+delete arr[1]; 
+delete arr[4]; 
+alert(arr.length);
