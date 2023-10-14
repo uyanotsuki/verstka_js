d3ef1f62c64474b51b3fd64dae3c67c1f26c6d59
kageyoshi <diana.azamatovva@gmail.com>
2023-10-14 18:16:19 +0300

    Задачи с формулами

---
+++ task67_practice_on_formulas/1.js
@@ -0,0 +1,34 @@
+"use strict";
+// 1 площадь круга
+let r = 4;
+let pi = 3.14;
+let s = pi * (r ** 2);
+alert(s);
+
+// 2 площадь квадрата
+ let a = 3;
+ let s1 = a*a;
+alert(s1);
+
+
+// 3 площадь прямоугольника
+let d = 2, b = 3;
+let ss = d* b;
+alert(ss);
+
+
+// 4 периметр прямоугольника
+let az = 2, bz = 3;
+let pz = 2 * (az+bz);
+alert(pz);
+
+// 5  перевод в градусы Фаренгейта
+let tc = 12;
+let tf = (tc*5/9) + 32;
+alert(tf);
+
+// 6  перевод в градусы Цельсия
+let tf2 = 40;
+let tc2 = (tf2-32) * 5/9;
+
+alert(tc2);
