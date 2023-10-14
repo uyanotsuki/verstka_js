5a2901c464c98bded42b29cd631d61c2a85ffe46
kageyoshi <diana.azamatovva@gmail.com>
2023-10-14 14:13:51 +0300

    Вывод текста в документ в JavaScript

---
+++ task64_document_write/1.js
@@ -0,0 +1,19 @@
+"use sctirct";
+// task 1
+document.write("task 1, some text" + '<br>');
+
+// task 2
+document.write(' <i>task 2</i>' + '<br>');
+
+// 3
+let str = 'text from task 3';
+document.write(' <i>'+ str + '</i>' + '<br>');
+
+// 
+document.write('1<br>');
+document.write('2<br>');
+document.write('3<br>');
+document.write('4<br>');
+document.write('5<br>');
+
+//Тег <br> Применяется, чтобы вставить в текст перенос строки, не создавая при этом абзац. Синтаксис: является одиночным тегом, то есть не имеет закрывающего тега.
