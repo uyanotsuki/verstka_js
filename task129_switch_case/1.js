e7c5b5ad28757a003b47b5aeda719b3d259b0ebb
kageyoshi <diana.azamatovva@gmail.com>
2023-11-11 20:12:12 +0300

    SWITCH-CASE

---
+++ task129_switch_case/1.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let lang = 'en';
+switch (lang) {
+  case 'ru':
+    console.log('рус');
+  break;
+  case 'en':
+    console.log('анг');
+  break;
+  case 'de':
+    console.log('нем');
+  break;
+  default:
+    console.log('язык не поддерживается');
+  break;
+}
