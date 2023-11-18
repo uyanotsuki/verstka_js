2a5ee0dc7a8441ce6e9df47ebee714b7bb8a318b
kageyoshi <diana.azamatovva@gmail.com>
2023-11-18 14:31:43 +0300

    Тернарны

---
+++ task131_tern_operator_ifelse/1.js
@@ -0,0 +1,17 @@
+// 1
+let num0 = 1;
+let res0;
+
+if (num >= 0) {
+	res0 = '1';
+} else {
+	res0 = '2';
+}
+
+console.log(res);
+
+// -->
+let num = 1;
+let res = num >= 0 ? '1' : '2';
+console.log(res);
+
---
+++ task132/1.js
@@ -0,0 +1,15 @@
+"use strict";
+// 1
+let a = 2 * (3 - 1);
+let b = 6 - 2;
+console.log(a == b);
+
+// 2
+let a2 = 5 * (7 - 4);
+let b2 = 1 + 2 + 7;
+console.log(a2 > b2);
+
+// 3
+let a3 = 2 ** 4;
+let b3 = 4 ** 2;
+console.log(a3 != b3);
