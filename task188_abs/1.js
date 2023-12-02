f36b39fd38d069faac78a6596cd61960683b47f9
whoyoshi <diana.azamatovva@gmail.com>
2023-12-02 17:43:56 +0300

    Модули

---
+++ task188_abs/1.js
@@ -0,0 +1,6 @@
+"use strict";
+// Даны переменные a и b. Найдите модуль разности a и b. Проверьте работу скрипта самостоятельно для различных a и b.
+let a = 5;
+let b = 8;
+let difference = Math.abs(a - b);
+console.log(difference); 
