36e57bf860d4799c667d88998d71645a83cd6b0e
whoyoshi <diana.azamatovva@gmail.com>
2023-12-02 21:09:27 +0300

    Граничные элементы

---
+++ task194/1.js
@@ -0,0 +1,19 @@
+"use strict";
+// 1  
+let a = [1, 2, 3];
+let nn = a.shift();
+console.log(nn);
+
+// 2
+let vv = a.pop();
+console.log(vv);
+
+// 3
+let b = [1, 2, 3];
+b.push(4, 5, 6);
+console.log(b);
+
+// 4
+let c = [1, 2, 3];
+c.unshift(4, 5, 6);
+console.log(c);
