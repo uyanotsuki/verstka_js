012f837865c6fc4568b9fb33114101f1072b0b26
whoyoshi <diana.azamatovva@gmail.com>
2023-12-02 18:04:07 +0300

    Регистр символов

---
+++ task189/1.js
@@ -0,0 +1,6 @@
+"use strict";
+// 1 Дана строка 'js'. Сделайте из нее строку 'JS'.
+// 2 Дана строка 'JS'. Сделайте из нее строку 'js'.
+let first = 'js', second = 'JS';
+console.log(first.toUpperCase());
+console.log(second.toLowerCase());
