36f91ae139da239bfd25c529cfa5fb1bffb9c0d7
whoyoshi <diana.azamatovva@gmail.com>
2023-12-03 17:51:28 +0300

    Свои функции

---
+++ task200_function/1.js
@@ -0,0 +1,16 @@
+"use strict";
+// 1  
+function yoshi() {
+  console.log('yoshi');
+}
+yoshi();
+
+// 2
+let sum = 0;
+function zxc() {
+  for (let i = 1; i <=100; i++){
+    sum += i;
+  }console.log((sum));
+}
+zxc();
+
