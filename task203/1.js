4b2337b9315e7b5c2319adba6d8e00367604d7a6
whoyoshi <diana.azamatovva@gmail.com>
2023-12-03 17:52:19 +0300

    Инструкция return

---
+++ task203/1.js
@@ -0,0 +1,10 @@
+"use strict";
+// 1  Сделайте функцию, которая параметрами принимает 3 числа и выводит в консоль сумму этих чисел.
+let param1 = 1;
+let param2 = 2;
+let param3 = 3;
+function func() {
+  console.log(param1 + param2 + param3);
+}
+func(param1, param2, param3);
+
