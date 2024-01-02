02b83e84b2905e4081a614727cb09e75e3dbd603
whoyoshi <diana.azamatovva@gmail.com>
2024-01-02 21:00:08 +0300

    Логические операторы без if в функциях

---
+++ task212/1.js
@@ -0,0 +1,20 @@
+"use strict";
+// Перепишите код в сокращенной форме согласно изученной теории.
+function func(a, b) {
+	return a == b;
+}
+
+// 2 
+function func2(a, b) {
+		return a != b;
+}
+
+// 3
+function func3(a, b) {
+		return a + b >= 10;
+}
+
+// 4
+function func4(a, b) {
+		return num >= 0;
+}
