324d2ea1d32dcea0a54f7d4857ab20635c59e499
whoyoshi <diana.azamatovva@gmail.com>
2024-01-03 19:48:11 +0300

    функция как переменная

--- task224/1.js
+++ task224/1.js
@@ -6,3 +6,19 @@ function func() {
 console.log(func); //3 
 func = '123';
 console.log(func);
+
+// 225
+function zxc() {
+  return 3;
+}
+let func2 = zxc;
+console.log(zxc() + func2());
+
+// 226
+let noname = function () {
+  return 1;
+}
+let noname2 = function () {
+  return 2;
+}
+alert(noname() + noname2());
