add7c0af6ee442526de5b9f616f54ddb9634ec08
whoyoshi <diana.azamatovva@gmail.com>
2024-01-07 21:24:21 +0300

    деструктуризация массивов

---
+++ task296/1.js
@@ -0,0 +1,7 @@
+"use strict";
+// 1 
+let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+let [name, surname, department, osition, salary] = arr; 
+
+console.log(name, surname, department, osition, salary);  
+ 
