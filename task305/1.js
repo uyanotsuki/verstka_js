94896a0c3b69d3a47920a8382e4c6cd71d2877f9
whoyoshi <diana.azamatovva@gmail.com>
2024-01-07 21:26:39 +0300

    Имена переменных при деструктуризации объектов

---
+++ task305/1.js
@@ -0,0 +1,13 @@
+"use strict";
+// 1 
+let options = {
+	color: 'red',
+	width:  400,
+	height: 500,
+};
+let {color: c, width: w, height: h} = options;
+
+console.log(c);  // выведет 2025
+console.log(w); // выведет 12
+console.log(h);   // выведет 31
+ 
