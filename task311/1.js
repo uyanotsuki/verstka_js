cd95107d68b55dc637e2d055e5f132f0cdd17f34
whoyoshi <diana.azamatovva@gmail.com>
2024-01-07 21:27:50 +0300

    Работа с объектом Date

---
+++ task311/1.js
@@ -0,0 +1,8 @@
+"use strict";
+// Выведите на экран текущий день.Выведите на экран текущий месяц. Выведите на экран текущий год.
+let date = new Date();
+console.log(date.getDate());     // день
+console.log(date.getMonth() + 1);    // месяц
+console.log(date.getFullYear()); // год
+ 
+ 
