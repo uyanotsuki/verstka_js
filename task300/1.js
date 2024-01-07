0b6fa3d4d9fb33ac027a43ed7cc9803e219f6af3
whoyoshi <diana.azamatovva@gmail.com>
2024-01-07 21:24:54 +0300

    остаток массива

---
+++ task300/1.js
@@ -0,0 +1,8 @@
+"use strict";
+// 1 
+let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+let [name, surname, department, position, salary] = arr; 
+let info = arr.slice(2);
+console.log(info);  
+ 
+ 
