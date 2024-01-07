055a757bf02249aca6c59e041bb9aea01c80041b
whoyoshi <diana.azamatovva@gmail.com>
2024-01-07 21:26:26 +0300

    деструктуризация объектов

---
+++ task304/1.js
@@ -0,0 +1,14 @@
+"use strict";
+// 1 
+let options = {
+	color: 'red',
+	width:  400,
+	height: 500,
+};
+let {color, width, height} = options;
+
+console.log(color);    // выведет 2025
+console.log(width);    // выведет 12
+console.log(height);   // выведет 31  
+ 
+ 
