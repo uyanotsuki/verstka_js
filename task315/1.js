8bd56559830f8af7483da2b3bf37a6f80e752b11
whoyoshi <diana.azamatovva@gmail.com>
2024-01-07 21:28:44 +0300

    Вывод частей даты словом

---
+++ task315/1.js
@@ -0,0 +1,12 @@
+"use strict";
+// 1 
+let date = new Date();
+let month  = date.getMonth();
+let months = [
+	'янв', 'фев', 'мар', 'апр', 'май', 'июн',
+	'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'
+];
+
+console.log(months[month]); 
+ 
+ 
