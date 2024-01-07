29eed5afe9d51dde1320b9d766791ce021e3ea27
whoyoshi <diana.azamatovva@gmail.com>
2024-01-07 21:24:32 +0300

    массив из функции

---
+++ task297/1.js
@@ -0,0 +1,9 @@
+"use strict";
+// 1 
+function func() {
+	return ['John', 'Smit', 'development', 'programmer', 2000];
+}
+let [name, surname, department, osition, salary] = func(); 
+console.log(func());  
+   
+ 
