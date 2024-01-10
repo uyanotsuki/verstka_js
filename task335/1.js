35ce343ff98f407406e3e69b0fd28d008740979d
whoyoshi <diana.azamatovva@gmail.com>
2024-01-10 20:51:46 +0300

    Попадание даты в промежуток

---
+++ task335/1.js
@@ -0,0 +1,68 @@
+"use strict";
+let date = '07-25';
+
+if (date >= '02-21' && date <= '03-20') {
+	console.log('Овен');
+}
+
+if (date >= '03-21' && date <= '04-21') {
+	console.log('Телец');
+}
+
+if (date >= '04-22' && date <= '05-21') {
+	console.log('Близнецы');
+}
+
+if (date >= '05-22' && date <= '06-22') {
+	console.log('Рак');
+}
+
+if (date >= '06-23' && date <= '07-23') {
+	console.log('Лев');
+}
+
+if (date >= '07-24' && date <= '08-23') {
+	console.log('Дева');
+}
+
+if (date >= '08-24' && date <= '09-23') {
+	console.log('Весы');
+}
+
+if (date >= '09-24' && date <= '10-22') {
+	console.log('Скорпион');
+}
+
+if (date >= '10-23' && date <= '11-22') {
+	console.log('Стрелец');
+}
+
+if (date >= '11-23' && date <= '00-20') {
+	console.log('Козерог');
+}
+
+if (date >= '00-21' && date <= '01-19') {
+	console.log('Водолей');
+}
+
+if (date >= '00-21' && date <= '01-19') {
+	console.log('Водолей');
+}
+
+if (date >= '01-20' && date <= '02-20') {
+	console.log('Рыбы');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
