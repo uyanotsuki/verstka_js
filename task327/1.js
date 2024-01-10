a0ebd7e35fd07b6df50f715706033fbc312c54ec
whoyoshi <diana.azamatovva@gmail.com>
2024-01-10 20:49:19 +0300

    Разность моментов

---
+++ task327/1.js
@@ -0,0 +1,10 @@
+"use strict";
+// 1 
+let date1 = new Date(2023, 8, 10);
+let date2 = new Date(2024, 0, 1); 
+console.log(Math.floor((date2 - date1)/(1000 * 60 * 60 * 24))); 
+
+// 2
+let date11 = new Date(2023, 10, 20);
+let date22 = new Date(2023, 11, 10); 
+console.log(Math.floor((date22 - date11)/(1000 * 60 * 60 * 24))); 
